c8d19199bb00245261046cd40152065ce118fbe6
felipe alves nunes <felipe.alvesnn@gmail.com>
2025-04-30 19:43:41 -0300

    feat: initialize Prisma schema with User and Role models
    - Added Prisma schema definition with User and Role models.
    - User model includes fields for id, name, email, isActive, createdAt, updatedAt, lastLogin, profileImageUrl, and a relation to roles.
    - Role model includes fields for id and name, with a relation to users.
    - Generated TypeScript definitions and JavaScript client for Prisma.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,16 @@
 export interface User {
-    id: number;
-    name: string;
+  id?: number;
+  name?: string;
+  email?: string;
+  roles?: Role[]; // Ex: ['admin', 'editor', 'viewer']
+  isActive?: boolean; // Está com a conta ativa?
+  createdAt?: Date; // Quando foi criado
+  updatedAt?: Date; // Última atualização
+  lastLogin?: Date; // Último login (opcional)
+  profileImageUrl?: string; // URL da imagem de perfil (opcional)
+}
+
+export interface Role {
+  id?: number;
+  name?: string;
 }
